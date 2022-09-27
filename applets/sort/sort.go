45f9796efd597db082085d9e716b5a7a7587e7d3
Miguel-Dorta <contact@migueldorta.com>
2022-09-27 23:00:41 +0100

    Add flag parsing to sort cmd

--- applets/sort/sort.go
+++ applets/sort/sort.go
@@ -2,15 +2,39 @@ package sort
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 	"sort"
 )
 
+type Option struct {
+	Help bool
+}
+
 const binaryName = "sort"
 
+func CreateFlagSet() (*flag.FlagSet, *Option) {
+	ret := flag.NewFlagSet(binaryName, flag.ExitOnError)
+	ret.Usage = func() {
+		fmt.Println(binaryName)
+		ret.PrintDefaults()
+	}
+	
+	var opt Option
+	ret.BoolVar(&opt.Help, "help", false, "display this help and exit")
+	return ret, &opt
+}
+
 func Main(stdout io.Writer, args []string) error {
+	flagSet, opt := CreateFlagSet()
+	flagSet.Parse(args)
+	if opt.Help {
+		flagSet.Usage()
+		return nil
+	}
+
 	lines := make([]string, 0)
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
